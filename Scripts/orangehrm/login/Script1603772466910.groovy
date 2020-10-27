c20340f3f135fb0f91867ccb92e7378f5d6d29ef
achmad mardiansyah <9075955+amardiansyah@users.noreply.github.com>
2020-10-27 11:36:51 +0700

    Update Script1603772466910.groovy
    menambahkan comment

--- Scripts/orangehrm/login/Script1603772466910.groovy
+++ Scripts/orangehrm/login/Script1603772466910.groovy
@@ -16,6 +16,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//open browser
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/')
